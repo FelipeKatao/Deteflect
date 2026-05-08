1854dd36faa2310bb6aa362001fcb8aeaca9fe66
FelipeKatao <cataofelipe@gmail.com>
2026-05-08 12:36:31 -0300

    Update .gitignore and enhance Model_train.txt: add model files to ignore list and correct entries in training data

--- Dataflect/pytest.py
+++ Dataflect/pytest.py
@@ -1,7 +1,4 @@
+from nlp.processIA.NplProcess import App
 
-from nlp.nlp_domain.npl_run import App
-from util.yamlParans import YamlParans
-p = App()
-v = YamlParans()
-print(v.GetParans("Deteflect.yaml"))
-#print(p.run("2","Inserir novo cliente nome guilherme idade 23"))
+v = App()
+v.run()
